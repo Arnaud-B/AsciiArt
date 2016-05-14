5446e82d40c27670d0cb36afa782a1fa28703706
Arnaud <arnaud.bouillod@live.fr>
2016-05-14 21:45:30 +0200

    Refactoring User Class

--- src/main/core/User.java
+++ src/main/core/User.java
@@ -1,6 +1,6 @@
 package main.core;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class User
 {
@@ -9,10 +9,8 @@ public class User
      *
      * @param args  the program's options if any
      */
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Text: ");
-        String text = in.nextLine();
+    public static void main(String[] args) {     
+    	String text = GenerateRandStr(); 	
         Game.getRandomPattern();
         Game.printPattern();
         System.out.println(text);
@@ -21,4 +19,19 @@ public class User
         Game.generateAsciiArt(text);
         Game.printAsciiArt();
     }
+    
+    public static String GenerateRandStr() {
+    	char[] chars = "abcdefghijklmnopqrstuvwxyz@יטאש.,;:!?".toCharArray();
+    	StringBuilder sb = new StringBuilder();
+    	Random random = new Random();
+    	Random rn = new Random();
+    	int numStr = rn.nextInt(20) + 1;
+    	for (int i = 0; i < numStr; i++) {
+    	    char c = chars[random.nextInt(chars.length)];
+    	    sb.append(c);
+    	}
+    	String text = sb.toString();
+		return text;
+    }
+    
 }
